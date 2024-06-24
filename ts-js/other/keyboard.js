9ef521f3869637a88096842842c12084e735e3d1
JustCreatedUser <168625934+JustCreatedUser@users.noreply.github.com>
2024-06-24 17:13:05 +0300

    Повна зміна, нажаль з файлами по jquery
    Разом з typescript змінилось і додалось ще багато чого

---
+++ ts-js/other/keyboard.js
@@ -0,0 +1,169 @@
+"use strict";
+import { music } from "./music.js";
+import { myCar } from "../mechanisms/cars.js";
+import { gearFunctions } from "../mechanisms/gearFunctions.js";
+import { rpmFunctions } from "../mechanisms/rpmFunctions.js";
+import { secondaryFunctions } from "./secondary.js";
+import { variables, permissions } from "./variables.js";
+export const keyboard = {
+  accelerate: "arrowup",
+  deccelerate: "arrowdown",
+  engine: "enter",
+  gearUp: "shift",
+  gearDown: "control",
+  goToMenu: "escape",
+  listenMusic: "m",
+};
+$(".keyName").click(function () {
+  if (!variables.choosingKeysMode) {
+    variables.choosingKeysMode = true;
+    variables.keyToChoose = $(this)[0].classList[1];
+    $(this).text("Нажми кнопку");
+    keyboard[variables.keyToChoose] = undefined;
+  }
+});
+$(".reset-keyboard").click(function () {
+  let response = confirm("Бажаєте скинути поточне управління до стандарту?");
+  if (response) {
+    let key;
+    for (key in keyboard) {
+      let text;
+      switch (key) {
+        case "accelerate":
+          text = "arrowup";
+          break;
+        case "deccelerate":
+          text = "arrowdown";
+          break;
+        case "gearDown":
+          text = "control";
+          break;
+        case "gearUp":
+          text = "shift";
+          break;
+        case "engine":
+          text = "enter";
+          break;
+        case "goToMenu":
+          text = "escape";
+          break;
+        case "listenMusic":
+          text = "m";
+          break;
+      }
+      keyboard[key] = text;
+      $("." + key).text(text);
+    }
+  }
+  localStorage.setItem("mini-car-racing-keyboard", JSON.stringify(keyboard));
+});
+function keyDown(e) {
+  if (!variables.choosingKeysMode && variables.device == "computer") {
+    let keyName;
+    if (e.code.includes("Key")) {
+      keyName = e.code.split("Key")[1].toLowerCase();
+    } else if (e.code == "Space") {
+      keyName = "space";
+    } else {
+      keyName = e.key.toLowerCase();
+    }
+    switch (keyName) {
+      case keyboard.accelerate:
+        if (!myCar.acceleration && !myCar.decceleration) {
+          myCar.acceleration = true;
+          if (
+            myCar.moveDirection != "acceleration" &&
+            !variables.isGamePaused &&
+            variables.isEngineWorking &&
+            permissions.forMoreRpm
+          ) {
+            myCar.moveDirection = `acceleration`;
+          }
+        }
+        break;
+      case keyboard.deccelerate:
+        if (
+          variables.isEngineWorking &&
+          !myCar.decceleration &&
+          !myCar.acceleration
+        ) {
+          if (
+            myCar.moveDirection != "decceleration" &&
+            variables.isEngineWorking &&
+            permissions.forLessRpm
+          ) {
+            myCar.moveDirection = `decceleration`;
+          }
+          myCar.decceleration = true;
+        }
+        break;
+      case keyboard.engine:
+        myCar.fns.useTheEngine();
+        break;
+      case keyboard.gearUp:
+        if (variables.isEngineWorking) {
+          gearFunctions.up.mechanism();
+        }
+        break;
+      case keyboard.gearDown:
+        if (myCar.gear != 0 && variables.isEngineWorking) {
+          gearFunctions.down.mechanism();
+        }
+        break;
+      case keyboard.goToMenu:
+        secondaryFunctions.pause();
+        break;
+      case keyboard.listenMusic:
+        music.listenToMusic();
+        break;
+    }
+  } else if (variables.device == "computer") {
+    let keyExists = Object.values(keyboard).filter(
+      (el) => el == e.key.toLowerCase()
+    );
+    if (keyExists.length != 0) {
+      alert("Ця кнопка вже використана");
+      return;
+    }
+    let name;
+    if (e.code.includes("Key")) {
+      name = e.code.split("Key")[1];
+    } else if (e.code == "Space") {
+      name = e.code;
+    } else {
+      name = e.key;
+    }
+    $("." + variables.keyToChoose).text(name.toLowerCase());
+    keyboard[variables.keyToChoose] = name.toLowerCase();
+    variables.keyToChoose = undefined;
+    variables.choosingKeysMode = false;
+    localStorage.setItem("mini-car-racing-keyboard", JSON.stringify(keyboard));
+  }
+}
+function keyUp(e) {
+  if (variables.device == "computer") {
+    let keyName =
+      e.code == "Space" ? e.code.toLowerCase() : e.key.toLowerCase();
+    if (
+      keyName == keyboard.accelerate &&
+      variables.action > 1 &&
+      myCar.acceleration
+    ) {
+      myCar.acceleration = false;
+      rpmFunctions.inertiaMechanism();
+      myCar.exhaust();
+      if (myCar.moveDirection !== 0) {
+        myCar.moveDirection = 0;
+        $(".car .vehicle").css("transform", "rotate(0)");
+      }
+    } else if (keyName == keyboard.deccelerate && myCar.decceleration) {
+      myCar.decceleration = false;
+      if (myCar.moveDirection !== 0) {
+        myCar.moveDirection = 0;
+        $(".car .vehicle").css("transform", "rotate(0)");
+      }
+    }
+  }
+}
+export { keyDown, keyUp };
+//# sourceMappingURL=keyboard.js.map
---
+++ ts-js/other/variables.js
@@ -0,0 +1,120 @@
+"use strict";
+export class StoryChanges {
+  constructor(obj) {
+    for (const key in obj) {
+      this[key] = obj[key];
+    }
+  }
+  getReady(changeWeather = false) {
+    for (const key in this) {
+      this[key] = false;
+    }
+    if (changeWeather) {
+      switch (this) {
+        case changes.introduction:
+        case changes.firstRace:
+          variables.race.style.backgroundImage =
+            "url(./icons-and-images/background-images/rain.jpg)";
+          variables.road.style.backgroundImage =
+            "url(./icons-and-images/roads/rain.png)";
+          $(".car .vehicle").attr(
+            "src",
+            "./icons-and-images/cars/myCar-lightened.png"
+          );
+          break;
+        case changes.secondRace:
+          variables.race.style.backgroundImage =
+            "url(./icons-and-images/background-images/day.jpg)";
+          variables.road.style.backgroundImage =
+            "url(./icons-and-images/roads/day.png)";
+          $(".car .vehicle").attr(
+            "src",
+            "./icons-and-images/cars/myCarDay.png"
+          );
+          break;
+        case changes.finalRace:
+          variables.race.style.backgroundImage =
+            "url(./icons-and-images/background-images/night.jpg)";
+          variables.road.style.backgroundImage =
+            "url(./icons-and-images/roads/night.png)";
+          $(".car .vehicle").attr(
+            "src",
+            "./icons-and-images/cars/myCar-lightened.png"
+          );
+          break;
+      }
+    }
+  }
+}
+export var variables = {
+    guideBlockText: document.querySelector(".logo"),
+    guideBlock: document.querySelector("header"),
+    road: document.querySelector(".road"),
+    race: document.getElementById("race"),
+    roadBox: document.querySelector(".bottom-road-box"),
+    car: document.querySelector(".car"),
+    finish: false,
+    device: undefined,
+    distanceRatio: window.innerHeight / -27,
+    turnValue: 0,
+    additionalMarginForTurn: 0,
+    choosingKeysMode: false,
+    keyToChoose: undefined,
+    $announcement: $('<div class="announcement"></div>')[0],
+    $announcementText: $('<h1 class="veryLargeText"></h1>')[0],
+    totalProgress: "introduction",
+    startInertiaMechanismTimeout: undefined,
+    hasChanged: undefined,
+    action: 0,
+    isEngineWorking: false,
+    raceBackgroundPositionX: 0,
+    backgroundPositionX: 0,
+    flame: undefined,
+    isGamePaused: false,
+    currentWindows: "race",
+    progress: "introduction",
+  },
+  permissions = {
+    forLessRpm: false,
+    forMoreRpm: false,
+    forInertia: false,
+    toOff_engine: false,
+    toSaveProgress: false,
+    setInertiaInterval: true,
+    toCheat: true,
+    toPause: false,
+  },
+  intervals = {
+    turnComing: undefined,
+    universalMoving: undefined,
+    inertia: undefined,
+    everyCarMove: undefined,
+    rpmIncreaseAnimation: undefined,
+    rpmDecreaseAnimation: undefined,
+    finishing: undefined,
+  },
+  changes = {
+    movingPause: false,
+    introduction: new StoryChanges({
+      first: true,
+      startRace: false,
+      useBrakesAction: false,
+      IntroDestionationPause: false,
+      gearDownAction: false,
+      reachIntroDestination: false,
+    }),
+    firstRace: new StoryChanges({
+      startRace: false,
+      continueFirstTurnExplanation: false,
+      firstTurnExplanation: false,
+    }),
+    secondRace: new StoryChanges({
+      startRace: false,
+      allowedToTurn: false,
+    }),
+    finalRace: new StoryChanges({
+      startRace: false,
+      allowedToTurn: false,
+    }),
+  };
+//# sourceMappingURL=variables.js.map
